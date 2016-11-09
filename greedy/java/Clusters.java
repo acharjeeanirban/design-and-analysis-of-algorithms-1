6ef03adf42b208d4f55d3d963c3caa959684d06e
Anirban Acharjee <anirbanacharjee@anirbans-air.attlocal.net>
2016-11-08 23:56:54 -0800

    finishing up clustering

--- greedy/java/Clusters.java
+++ greedy/java/Clusters.java
@@ -40,98 +40,98 @@ class VerVerEdgeComparator implements Comparator<VerVerEdge> {
 	}
 }
 
+class VerVerEdgeComparator1 implements Comparator<VerVerEdge> {
+	public int compare(VerVerEdge first, VerVerEdge second) {
+		//return second.getEdge() - first.getEdge();
+		return first.getEdge() - second.getEdge();
+	}
+}
+
 class Clusters {
 
 	HashMap<Integer, ArrayList<AdjVerEdge>> graph = new HashMap<>();
 	Set<Integer> totalNumberOfVerts = new HashSet<Integer>();
 	PriorityQueue<VerVerEdge> queue = new PriorityQueue<VerVerEdge>(new VerVerEdgeComparator());
+	PriorityQueue<VerVerEdge> queue1 = new PriorityQueue<VerVerEdge>(new VerVerEdgeComparator1());
 
 
 
 	public void doClustering(int k) {
+		VerVerEdge obj1 = queue1.poll();
+		System.out.println("first = " + obj1.getVer1() + " second " + obj1.getVer2());
+		System.out.println("edge = "  + obj1.getEdge());
 		ArrayList<ArrayList<Integer>> clusters = new ArrayList<ArrayList<Integer>>();
 		for (int in : totalNumberOfVerts) {
 			ArrayList<Integer> list = new ArrayList<Integer>();
 			list.add(in);
 			clusters.add(list);
 		}
-
-		System.out.println("bazinga = " + clusters);
-		//clusters.remove(0);
-		System.out.println("bazinga = " + clusters);
-		ArrayList<ArrayList<Integer>> test = new ArrayList<ArrayList<Integer>>();
-		ArrayList<Integer> test1 = new ArrayList<Integer>();
-		test1.add(1);
-		test.add(test1);
-		System.out.println(test);
-		test.remove(0);
-		System.out.println(test);
-		System.out.println("The initial number of clusters = " + clusters.size());
-		System.out.println("The initial number of clusters = " + queue.size());
-		
+						
 		ArrayList<Integer> listOfIndexes = new ArrayList<Integer>();
 		int count = 1;
 
 		//while (clusters.size() != k) {
-			while (!queue.isEmpty()) {
-				VerVerEdge obj = queue.poll();
-				int first = obj.getVer1();
-				int second = obj.getVer2();
-				
-				ArrayList<Integer> newCluster = new ArrayList<Integer>();
-				for (int i = 0; i < clusters.size(); i++) {
-					if (clusters.get(i).contains(first) || clusters.get(i).contains(second)) {
-						//System.out.println(count);
-						//newCluster.addAll(clusters.get(i));
-						listOfIndexes.add(i);
-					}
+		while (!queue.isEmpty()) {
+			VerVerEdge obj = queue.poll();
+			int first = obj.getVer1();
+			int second = obj.getVer2();
+			
+			ArrayList<Integer> newCluster = new ArrayList<Integer>();
+			for (int i = 0; i < clusters.size(); i++) {
+				if (clusters.get(i).contains(first) || clusters.get(i).contains(second)) {
+					listOfIndexes.add(i);
 				}
-				System.out.println("listOfIndexes = " + listOfIndexes);
-
-				for (int in : listOfIndexes) {
-					newCluster.addAll(clusters.get(in));
-				}
-
-				System.out.println("clusters1 = " + clusters);
-
-				int k1 = 0;
-				int flag = 0;
-				for (int in : listOfIndexes) {
-					if (flag == 0) {
-						clusters.remove(in);
-						flag = 1;
-						//k = in;	
-					} else {
-						k1 = in;
-						clusters.remove(--k1);
-					}
-					
-					//System.out.println("Ttttttttt " + in);
-					
-				}
-
-				System.out.println("clusters2 = " + clusters);
-
-				listOfIndexes.clear();
-				//System.out.println("the size of listOfIndexes = " + listOfIndexes.size());
-
-				clusters.add(newCluster);
-				//newCluster.clear();
-
-				System.out.println("clusters3 = " + clusters);
-
-				System.out.println("==================================================");				
-
-				if (clusters.size() == k) {
-					break;
-				}
-				count++;
+			}
+			
+			for (int in : listOfIndexes) {
+				newCluster.addAll(clusters.get(in));
 			}
 
+			
 
-		//}
+			int k1 = 0;
+			int flag = 0;
+			for (int in : listOfIndexes) {
+				if (flag == 0) {
+					clusters.remove(in);
+					flag = 1;
+					//k = in;	
+				} else {
+					k1 = in;
+					clusters.remove(--k1);
+				}
+				
+			}
 
-		System.out.println("ALL DONE " + clusters);
+			listOfIndexes.clear();
+
+			clusters.add(newCluster);			
+
+			if (clusters.size() == k) {
+				break;
+			}
+			count++;
+		}
+
+		int distance = 0;
+
+		while (!queue1.isEmpty()) {
+			VerVerEdge vve = queue1.poll();
+
+
+			for (ArrayList<Integer> in : clusters) {
+				if (in.contains(vve.getVer1()) && !in.contains(vve.getVer2())) {
+					distance = vve.getEdge();
+					break;
+				}
+			}
+
+			if (distance != 0) {
+				break;
+			}
+
+		}
+		System.out.println("maximum spacing : " + distance);
 
 	}
 
@@ -149,6 +149,7 @@ class Clusters {
 					int third = Integer.parseInt(str.substring(str.lastIndexOf(" ") + 1));
 					VerVerEdge obj = new VerVerEdge(first, second, third);
 					queue.offer(obj);
+					queue1.offer(obj);
 
 
 					AdjVerEdge adjVerEdge1 = null;
@@ -185,6 +186,6 @@ class Clusters {
 	public static void main(String[] args) {
 		Clusters clusters = new Clusters();
 		clusters.readFile();
-		clusters.doClustering(3);
+		clusters.doClustering(4);
 	}
 }
