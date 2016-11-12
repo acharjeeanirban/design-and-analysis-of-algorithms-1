31eeb0ffaa8c346e2188e6593f86685bbf6b4a53
Anirban Acharjee <aacharjee@apple.com>
2016-11-11 23:43:47 -0800

    progress on hamming

--- greedy/java/Hamming.java
+++ greedy/java/Hamming.java
@@ -4,6 +4,40 @@ import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.util.stream.Stream;
 
+class HammingNode {
+
+	String value;
+	HammingNode leader;
+
+	HammingNode(String value) {
+		this.value = value;
+		leader = this;
+	}
+
+	public void setLeader(HammingNode leader) {
+		this.leader = leader;
+	}
+
+	public String getValue() {
+		return value;
+	}
+
+	public HammingNode getLeader() {
+		return leader;
+	}
+
+	public boolean equals(Object obj) {
+		HammingNode node = (HammingNode) obj;
+		return (node.value.equals(this.value));
+	}
+
+	public int hashCode() {
+		return value.length();
+	}
+
+}
+
+
 class Hamming {
 
 	public int getDistance(String s1, String s2) {
@@ -19,33 +53,77 @@ class Hamming {
 		return distance;
 	}
 
+	public ArrayList<String> getOneOrTwoAway(String input) {
+		ArrayList<HammingNode> list = new ArrayList<HammingNode>();
+		char arr[] = input.toCharArray();
+		StringBuffer sb = new StringBuffer();
+		char changeI = 'a';
+		HammingNode newNode = null;
+		
+		for (int i = 0; i < arr.length; i++) {
+			if (arr[i] == '1') {
+				sb.append(input.substring(0,i)).append('0').append(input.substring(i+1));
+			} else {
+				sb.append(input.substring(0,i)).append('1').append(input.substring(i+1));
+			}
+			newNode = new HammingNode(sb.toString());
+			if (listOfHammingNodes.contains(newNode) {
+
+			}
+			list.add(newNode);
+			sb.setLength(0);
+		}
+	
+		for (int i = 0; i < arr.length-1; i++) {
+
+			if (arr[i] == '1') {
+					changeI = '0';
+			} else {
+					changeI = '1';
+			}
+
+			for (int j = i+1; j < arr.length; j++) {
+				if (arr[j] == '1') {
+					sb.append(input.substring(0,i)).append(changeI).append(input.substring(i+1, j)).append('0').append(input.substring(j+1));
+				} else {
+					sb.append(input.substring(0,i)).append(changeI).append(input.substring(i+1, j)).append('1').append(input.substring(j+1));
+				}
+				newNode = new HammingNode(sb.toString());
+
+				list.add(newNode);
+				sb.setLength(0);		
+			}
+				
+		}
+		
+		return list;
+	}
+
+	List<HammingNode> listOfHammingNodes = new ArrayList<>();
 	public void readFile() {
 		String fileName = "/Users/anirbanacharjee/Documents/design-and-analysis-of-algorithms-1/greedy/clustering_big.txt";
 		List<String> list = new ArrayList<>();
-		List<String> list1 = new ArrayList<>();
+		
 		StringBuffer sb = new StringBuffer();
+		String origin = "000000000000000000000000";
+		ArrayList<ArrayList<String>> clusters = new ArrayList<ArrayList<String>>();
 		try (Stream<String> stream = Files.lines(Paths.get(fileName))) {
 		//try (Stream<String> stream = Files.lines(Paths.get(fileName))) {
 			stream.forEach(s -> list.add(s));
 			int flag = 0;
 			for (String str : list) {
 				if (flag != 0) {
-					// int first = Integer.parseInt(str.substring(0,str.indexOf(" ")));
-					// int second = Integer.parseInt(str.substring(str.indexOf(" ") + 1, str.lastIndexOf(" ")));
-					// int third = Integer.parseInt(str.substring(str.lastIndexOf(" ") + 1));
-					// VerVerEdge obj = new VerVerEdge(first, second, third);
-					// queue.offer(obj);
-					// queue1.offer(obj);
-					// totalNumberOfVerts.add(first);
-					// totalNumberOfVerts.add(second);
 
 					String arr[] = str.split(" ");
 					for (String s : arr) {
 						sb.append(s);
-						//System.out.print(s);
 					}
 
-					list1.add(sb.toString());
+					String newNode = sb.toString();
+					HammingNode hammingNode = new HammingNode(newNode);
+					listOfHammingNodes.add(hammingNode);
+
+					 sb.setLength(0);
 
 					//System.out.println();
 
@@ -56,32 +134,24 @@ class Hamming {
 			ioException.printStackTrace();
 		}
 
-		int count = 0;
-		Set<String> set = new HashSet<>();
-		ArrayList<ArrayList<String>> biglist = new ArrayList<ArrayList<String>>();
-		for (int i = 0; i < list1.size(); i++) {
-			//count++;
-			for (int j = i+1; j < list1.size(); j++) {
-				String s1 = list1.get(i);
-				String s2 = list1.get(j);
-				if (getDistance(s1, s2) <= 2) {
-					set.add(s1);
-					set.add(s2);
-				}
-			}
-
-
-		}
-
 		System.out.println("DONE");
 
+	}
 
+	public void doUF() {
+		for (HammingNode node : listOfHammingNodes) {
+			for (HammingNode neighbours : getOneOrTwoAway(node.getValue())) { 
 
+			}
+		}
 	}
 
 	public static void main(String[] args) {
 		Hamming hamming = new Hamming();
 		hamming.readFile();
-		System.out.println(hamming.getDistance("001", "111"));
+		//System.out.println(hamming.getDistance("001", "111"));
+
+		System.out.println(hamming.getOneOrTwoAway("1000"));
+
 	}
 } 
